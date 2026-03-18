fece215b31100d795c766050d992f72531d8f0cd
lavXaga <lavragabro@gmail.com>
2026-03-18 14:32:46 +1000

    Create проверка.c

---
+++ делал для себя /проверка.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // This is a simple C program
+    // It asks for your name and age
+    
+    char name[50];
+    int age;
+    
+    printf("Enter your name: ");
+    scanf("%s", name);
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    printf("\nHello %s!\n", name);
+    printf("You are %d years old.\n", age);
+    
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a minor.\n");
+    }
+    
+    return 0;
+}
