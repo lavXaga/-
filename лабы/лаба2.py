dd7768ebb16804a6efeff7ff73831c7e506685dc
lavXaga <lavragabro@gmail.com>
2026-03-18 14:25:57 +1000

    Update лаба2.py

--- лабы/лаба2.py
+++ лабы/лаба2.py
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // ЧАСТЬ 1: Матрица 3x3 (double)
+    // PART 1: Matrix 3x3 (double)
     double matrix3x3[3][3];
     double main_diag_sum = 0, secondary_diag_sum = 0;
     int i, j;
 
-    printf("Введите элементы матрицы 3x3 (double):\n");
+    printf("Enter elements of 3x3 matrix (double):\n");
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             printf("matrix3x3[%d][%d] = ", i, j);
@@ -20,14 +20,14 @@ int main() {
         secondary_diag_sum += matrix3x3[i][2 - i];     
     }
 
-    printf("\nСумма главной диагонали: %.2f\n", main_diag_sum);
-    printf("Сумма побочной диагонали: %.2f\n", secondary_diag_sum);
+    printf("\nSum of main diagonal: %.2f\n", main_diag_sum);
+    printf("Sum of secondary diagonal: %.2f\n", secondary_diag_sum);
 
-    // ЧАСТЬ 2: Матрица 2x2 (int) и её квадрат
+    // PART 2: Matrix 2x2 (int) and its square
     int matrix2x2[2][2];
     int result[2][2];
 
-    printf("\nВведите элементы матрицы 2x2 (int):\n");
+    printf("\nEnter elements of 2x2 matrix (int):\n");
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++) {
             printf("matrix2x2[%d][%d] = ", i, j);
@@ -41,9 +41,10 @@ int main() {
     result[1][0] = matrix2x2[1][0] * matrix2x2[0][0] + matrix2x2[1][1] * matrix2x2[1][0];
     result[1][1] = matrix2x2[1][0] * matrix2x2[0][1] + matrix2x2[1][1] * matrix2x2[1][1];
 
-    printf("\nКвадрат матрицы:\n");
+    printf("\nSquare of the matrix:\n");
     printf("%d %d\n", result[0][0], result[0][1]);
     printf("%d %d\n", result[1][0], result[1][1]);
 
     return 0;
 }
+```
