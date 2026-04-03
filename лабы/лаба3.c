22ec4079c4ae3e207cef200930a5d3931efb774a
lavXaga <lavragabro@gmail.com>
2026-04-03 21:02:45 +1000

    Update лаба3.c

--- лабы/лаба3.c
+++ лабы/лаба3.c
@@ -7,27 +7,28 @@ int main() {
     int i;
     
     do {
-        printf("\nВведите строку (до 80 символов): ");
+        printf("\nEnter a string (up to 80 characters): ");
         fgets(str, sizeof(str), stdin);
         size_t len = strlen(str);
         if (len > 0 && str[len-1] == '\n') {
             str[len-1] = '\0';
-        }
+        } else {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+        } 
         for (i = 0; str[i] != '\0'; i++) {
-            if (str[i] == 'а') {
-                str[i] = 'А';
-            } else if (str[i] == 'ь') {
-                str[i] = 'Ь';
+            if (str[i] == 'a') {
+                str[i] = 'A';
+            } else if (str[i] == 'b') {
+                str[i] = 'B';
             }
         }
-        
-        printf("Результат: %s\n", str);
-        
-        printf("\nХотите продолжить? (y/n): ");
+        printf("Result: %s\n", str);
+        printf("\nContinue? (y/n): ");
         scanf(" %c", &choice);
-        
-    } while (choice == 'y' || choice == 'Y');
+        while (getchar() != '\n');
     
-    printf("Программа завершена.\n");
+    } while (choice == 'y' || choice == 'Y');
+    printf("Program terminated.\n");
     return 0;
 }
