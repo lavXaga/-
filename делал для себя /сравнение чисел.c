44b202ff6919b0b470943b2f0eb16540deeb9a7c
lavXaga <lavragabro@gmail.com>
2026-04-06 00:32:40 +1000

    Create сравнение чисел.c

---
+++ делал для себя /сравнение чисел.c
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int nom;
+    int num;
+    std::cout << "Введите число nom: ";
+    std::cin >> nom;
+    std::cout << "Введите число num: ";
+    std::cin >> num;
+    if (num > nom) {
+        std::cout<<"число "<<num<<" определенно больше "<<nom;
+        if (num < nom){
+
+            std::cout<<" число "<<num<<" определенно меньше "<<nom;
+        }
+    }
+    else {
+        std::cout<<" число "<<num<<" = "<<nom;
+    }
+
+
+    return 0;
+}
