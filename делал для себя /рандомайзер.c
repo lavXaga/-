35955da1d82b720064f12e35e1437a0aa5fbfc64
lavXaga <lavragabro@gmail.com>
2026-04-06 23:22:27 +1000

    Update рандомайзер.c

--- делал для себя /рандомайзер.c
+++ делал для себя /рандомайзер.c
@@ -3,13 +3,11 @@
 #include <time.h>
 
 int main() {
-    // Инициализируем генератор случайных чисел текущим временем
     srand(time(NULL));
     
     // Генерируем случайное число от 0 до 32767
     int random_number = rand();
     
-    // Выводим результат
     printf("Случайное число: %d\n", random_number);
 
     return 0;
