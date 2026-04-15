7889113722f7c93012db4ecc7e73ef92c3342155
lavXaga <lavragabro@gmail.com>
2026-04-15 20:26:26 +1000

    Update заголовочный.h

--- лабы/4лаба/заголовочный.h
+++ лабы/4лаба/заголовочный.h
@@ -9,4 +9,4 @@ double calculate_perimeter(double a, double b, double c);
 double calculate_area(double a, double b, double c);
 int is_valid_triangle(double a, double b, double c);
  
-#endif
+#endif 
