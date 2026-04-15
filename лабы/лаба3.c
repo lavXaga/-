c42c8525a03c6c1ecfe2725bf6c4a6682359aad7
lavXaga <lavragabro@gmail.com>
2026-04-15 21:32:50 +1000

    Update лаба3.c

--- лабы/лаба3.c
+++ лабы/лаба3.c
@@ -19,7 +19,7 @@ int main() {
         for (i = 0; str[i] != '\0'; i++) { 
             if (str[i] == 'a') {
                 str[i] = 'A';
-            } else if (str[i] == 'b') {
+            } else if (str[i] == 'b') { 
                 str[i] = 'B';
             }
         }
