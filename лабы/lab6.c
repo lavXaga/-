ef766098140104cab18d67fad8cfffe166456d36
lavXaga <lavragabro@gmail.com>
2026-05-15 19:39:13 +1000

    Implement data filtering in lab6.c
    Read data from input.txt and filter based on year and name criteria, writing results to output.txt.

---
+++ лабы/lab6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "windows.h"
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    FILE *in=fopen("input.txt","r");
+    FILE *out=fopen("output.txt","w");
+    char name[100];
+    char surname[100];
+    char patr[100];
+    int year;
+    while (fscanf(in,"%s %s %s %d ", surname, name , patr, &year)==4) {
+        if (year >1980){
+            fprintf(out,"%s %s %s %d\n", surname,name, patr, year);
+        }
+    }
+    while (fscanf(in,"%s %s %s %d ", surname, name , patr, &year)==4) {
+        if (name[0] == 'Б') {
+            fprintf(out,"%s %s %s %d\n", surname,name, patr, year);
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 0;
+}
