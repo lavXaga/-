b5bb8b6256dcec3633b14247291a442ec3adf615
lavXaga <lavragabro@gmail.com>
2026-05-29 18:35:40 +1000

    Create лаба 15.c

---
+++ лабы/лаба 15.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "windows.h"
+#include "stdlib.h"
+#include "string.h"
+
+void p(int n, int m) {
+    if (n<10)
+        printf("%d ",n);
+    else if (m==0) {
+        p(n/10, 0);
+        printf("%d ",n%10);
+    }
+    else {
+        printf("%d ",n%10);
+        p(n/10, 1);
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n;
+    printf("число: ");
+    scanf("%d",&n);
+    p(n,0);
+    printf("\n");
+    p(n,1);
+    printf("\n");
+
+    return 0;
+}
+
